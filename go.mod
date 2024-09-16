21ce36543c30dc53b15acdce705a560b37ff9f78
Ev357 <evest.dev@gmail.com>
2024-09-16 18:45:01 +0200

    feat: timetable parsing

--- go.mod
+++ go.mod
@@ -3,6 +3,13 @@ module github.com/Ev357/bakalari-calendar-sync
 go 1.23.0
 
 require (
-	github.com/joho/godotenv v1.5.1 // indirect
-	golang.org/x/net v0.29.0 // indirect
+	github.com/go-shiori/dom v0.0.0-20230515143342-73569d674e1c
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/net v0.29.0
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/gogs/chardet v0.0.0-20211120154057-b7413eaefb8f // indirect
+	golang.org/x/text v0.18.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 	"os"
+	"strconv"
+	"time"
 
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
 
+	"github.com/go-shiori/dom"
 	"github.com/joho/godotenv"
+	"golang.org/x/net/html"
 )
 
 func main() {
@@ -19,38 +23,238 @@ func main() {
 	config, err := getConfig()
 
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		panic(err)
 	}
 
 	loginUrl := config.url + "/bakaweb/Login"
 
 	jar, err := cookiejar.New(nil)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		panic(err)
 	}
 
 	client := &http.Client{
 		Jar: jar,
 	}
 
-	resp, err := client.PostForm(loginUrl, url.Values{
+	_, err = client.PostForm(loginUrl, url.Values{
 		"username":   {config.username},
 		"password":   {config.password},
 		"persistent": {"true"},
 	})
 
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		panic(err)
+	}
+
+	resp, err := client.Get(config.url + "/bakaweb/next/rozvrh.aspx")
+
+	if err != nil {
+		panic(err)
 	}
 
 	defer resp.Body.Close()
 
-	content, _ := io.ReadAll(resp.Body)
+	doc, err := html.Parse(resp.Body)
 
-	fmt.Println(string(content))
+	if err != nil {
+		panic(err)
+	}
+
+	timetable := dom.GetElementByID(doc, "schedule")
+
+	hours, err := getHours(timetable)
+
+	if err != nil {
+		panic(err)
+	}
+
+	days, err := getDays(timetable, hours)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(days)
+
+}
+
+type Class struct {
+	num     int
+	from    time.Time
+	to      time.Time
+	date    time.Time
+	teacher string
+	room    string
+	name    string
+	status  string
+}
+
+type Day []Class
+
+func getDays(timetable *html.Node, hours []Hour) ([]Day, error) {
+	days := []Day{}
+
+	for _, dayContainer := range dom.GetElementsByClassName(timetable, "day-row") {
+		day, err := getDay(dayContainer, hours)
+
+		if err != nil {
+			return nil, err
+		}
+
+		days = append(days, *day)
+	}
+
+	return days, nil
+}
+
+func getDay(doc *html.Node, hours []Hour) (*Day, error) {
+	day := Day{}
+
+	dateNode := dom.QuerySelector(doc, ".day-name > div > span")
+
+	currentYear := time.Now().Year()
+	parsedDate, err := time.Parse("2/1", dom.InnerText(dateNode))
+	if err != nil {
+		return nil, err
+	}
+
+	date := time.Date(currentYear, parsedDate.Month(), parsedDate.Day(), 0, 0, 0, parsedDate.Nanosecond(), parsedDate.Location())
+
+	for index, classContainer := range dom.QuerySelectorAll(doc, ".day-row > div > div > span") {
+		class, err := getClass(classContainer, index, hours, date)
+
+		if err != nil {
+			return nil, err
+		}
+
+		day = append(day, *class)
+	}
+
+	return &day, nil
+}
+
+func getClass(node *html.Node, index int, hours []Hour, date time.Time) (*Class, error) {
+	emptyNode := dom.QuerySelector(node, ".empty")
+
+	dayItemNode := dom.QuerySelector(node, "div[data-detail]")
+
+	type DataDetail struct {
+		Type    string `json:"type"`
+		Teacher string `json:"teacher"`
+		Room    string `json:"room"`
+	}
+	dataDetail := DataDetail{}
+
+	if dayItemNode != nil {
+		for _, attr := range dayItemNode.Attr {
+			if attr.Key == "data-detail" {
+				err := json.Unmarshal([]byte(attr.Val), &dataDetail)
+
+				if err != nil {
+					return nil, err
+				}
+
+			}
+		}
+	}
+
+	if dataDetail.Type == "removed" {
+		return &Class{
+			num:    hours[index].num,
+			from:   hours[index].from,
+			to:     hours[index].to,
+			date:   date,
+			status: "removed",
+		}, nil
+	}
+
+	if emptyNode != nil {
+		return &Class{
+			num:    hours[index].num,
+			from:   hours[index].from,
+			to:     hours[index].to,
+			date:   date,
+			status: "empty",
+		}, nil
+	}
+
+	nameNode := dom.QuerySelector(node, ".day-item > div > div > div:nth-child(2)")
+
+	if nameNode == nil {
+		return nil, errors.New("Class name not found")
+	}
+
+	name := dom.InnerText(nameNode)
+
+	return &Class{
+		num:     hours[index].num,
+		from:    hours[index].from,
+		to:      hours[index].to,
+		date:    date,
+		teacher: dataDetail.Teacher,
+		room:    dataDetail.Room,
+		name:    name,
+		status:  "normal",
+	}, nil
+}
+
+func getHours(timetable *html.Node) ([]Hour, error) {
+	hoursContainer := dom.GetElementByID(timetable, "hours")
+
+	hours := []Hour{}
+
+	for _, hourContainer := range dom.GetElementsByClassName(hoursContainer, "item") {
+		hour, err := getHourData(hourContainer)
+
+		if err != nil {
+			return nil, err
+		}
+
+		hours = append(hours, *hour)
+	}
+
+	return hours, nil
+}
+
+type Hour struct {
+	num  int
+	from time.Time
+	to   time.Time
+}
+
+func getHourData(doc *html.Node) (*Hour, error) {
+	numNode := dom.QuerySelector(doc, ".num")
+
+	num, err := strconv.Atoi(dom.InnerText(numNode))
+
+	if err != nil {
+		return nil, err
+	}
+
+	const layout = "15:04"
+
+	fromNode := dom.QuerySelector(doc, ".from")
+
+	from, err := time.Parse(layout, dom.InnerText(fromNode))
+
+	if err != nil {
+		return nil, err
+	}
+
+	toNode := dom.QuerySelector(doc, ".to")
+
+	to, err := time.Parse(layout, dom.InnerText(toNode))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Hour{
+		num,
+		from,
+		to,
+	}, nil
 }
 
 type Config struct {
